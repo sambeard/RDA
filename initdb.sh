99a589d15e207151eb8112396664d42e7a6d49bd
Sam Beard <sam.beard@zelezny.nl>
2019-07-29 17:04:42 +0200

    update db init script

--- initdb.sh
+++ initdb.sh
@@ -1,3 +1,19 @@
 #!/usr/bin/bash
+
+# remove db
 echo "Removing database if exists"
 rm -f rda.db
+
+# create db
+echo "Creating new database rda.db"
+sqlite3 rda.db <<EOF
+.exit
+EOF
+
+# fill db
+echo "Importing data from data/worldcities.csv"
+sqlite3 rda.db <<EOF
+.mode csv
+.import .\data\worldcities.csv cities
+.schema cities
+EOF
